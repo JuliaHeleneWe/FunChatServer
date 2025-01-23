0b0bf46ebf40c2bfa3f0fb3005c8e6802f75943a
JuliaHeleneW <gyros.chips@gmail.com>
2025-01-23 22:17:10 +0100

    Initial simple socket server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http=require('http');
+const socketio=require('socket.io');
+
+const app=express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection',socket =>{
+    console.log('New user connection');
+
+    socket.on('message',(user,msg,room) =>{
+        //store data in DB
+        io.emit('message', user,msg,room);
+    });
+
+    socket.on('joinRoom',(user,room) =>{
+        io.emit('joinRoom', user, room);
+    });
+    socket.on('disconnect',() =>{
+        console.log('user disconnected');
+    });
+});
+
+const PORT=3000||process.env.PORT;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));

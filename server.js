87e2ca6ac3669d3589cd83e20720b621c3662a78
JuliaHeleneW <gyros.chips@gmail.com>
2025-01-27 19:43:06 +0100

    Added https with self-signed certificate

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 const express = require('express');
-const http=require('http');
+const https=require('https');
 const socketio=require('socket.io');
+const fs=require('fs');
 
-const app=express();
-const server = http.createServer(app);
+
+const cert = fs.readFileSync("./prod/cert.pem");
+const key = fs.readFileSync("./prod/key.pem");
+const app = express();
+const server = https.createServer({
+    key: key,
+    cert:cert,
+    requestCert: true,
+    rejectUnauthorized: false
+}, app)
 const io = socketio(server);
+
+//TODO: header-Abgleich für socket.handshake.headers
+
 
 io.on('connection',socket =>{
     console.log('New user connection');
 
     socket.on('message',(user,msg,room) =>{
         //store data in DB
-        io.emit('message', user,msg,room);
-    });
-
-    socket.on('joinRoom',(user,room) =>{
-        io.emit('joinRoom', user, room);
+        if(!msg.includes('spawn') && !msg.includes('ciao')){
+            io.emit('message', user,msg,room);
+        }
+        else {
+            io.emit('msgError', msg, 'Keine Macht den Bots!');
+        }
     });
     socket.on('disconnect',() =>{
         console.log('user disconnected');
+    });
+    socket.on('connect_error',(err) =>{
+        console.log(err);
     });
 });
 
